b5dc626c2bb974f35bf184b442b0402ad3e5d9cc
Abhi Sheshagiri <abhi.sheshagiri@parkmobile.io>
2023-03-06 17:50:24 +0530

    changes

--- RSR-SDK/src/main/java/com/example/pm/rsrsdk/ListView.kt
+++ RSR-SDK/src/main/java/com/example/pm/rsrsdk/ListView.kt
@@ -2,26 +2,40 @@ package com.example.pm.rsrsdk
 
 import android.os.Bundle
 import android.widget.ArrayAdapter
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class ListView: AppCompatActivity() {
+    private val dummyList = intent.getStringArrayListExtra("listDummy")
+    private val dummyString = intent.getStringExtra("stringDummy")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.list_view)
 
         val arrayAdapter: ArrayAdapter<*>
-        val users = arrayOf(
-            "One", "Two", "Three",
-            "Four", "Five", "Six", "Seven", "Eight",
-            "Nine", "Ten"
-        )
 
-        var mListView = findViewById<android.widget.ListView>(R.id.userlist)
+//        Before
+
+//        val users = arrayOf(
+//            "One", "Two", "Three",
+//            "Four", "Five", "Six", "Seven", "Eight",
+//            "Nine", "Ten"
+//        )
+
+//        After
+
+        val users = arrayOf(dummyList)
+
+        val mListView = findViewById<android.widget.ListView>(R.id.userlist)
         arrayAdapter = ArrayAdapter(
             this,
             android.R.layout.simple_list_item_1, users
         )
         mListView.adapter = arrayAdapter
+
+        val dummyTextView = findViewById<TextView>(R.id.testName)
+        dummyTextView.text = dummyString
+
     }
 }
